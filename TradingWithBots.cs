1f26ca004199b75fc8fd5f92ccee4e6a8f160e46
noHero <warseppl@googlemail.com>
2014-05-13 09:17:25 +0200

    changed profileid of bot to the new one

--- TradingWithBots.cs
+++ TradingWithBots.cs
@@ -11,7 +11,7 @@ namespace Auction.mod
 
         //for faster testing some variables:
         public string botname ="auctionmod";
-        public int botid = 123;
+        public int botid = 13754;
         public string oldbotid = "1b8a4125d2634634aa76f33e1d04b0d4";
         public string spreadsheet ="0AhhxijYPL-BGdHBDYzRLcDhFU2FJZkVEaWNFREdaLUE";
 
